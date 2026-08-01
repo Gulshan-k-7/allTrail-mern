ccf997755b40da49db784ea4f61d4a17120fcecf
Gulshan-k-7 <gulshangk7878@gmail.com>
2026-08-01 15:57:57 +0530

    Update Community.jsx

--- client/src/Pages/Community.jsx
+++ client/src/Pages/Community.jsx
@@ -13,8 +13,8 @@ import {
     Users,
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+const API_URL = import.meta.env.VITE_API_URL;
 
-const API_URL = `${import.meta.env.VITE_API_URL}/api/posts`;
 
 function Community() {
     const navigate = useNavigate();
